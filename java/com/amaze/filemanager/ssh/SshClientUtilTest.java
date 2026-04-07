9197e4b17e2381c82f79969ee221fba365094cb6
ClanVv3 <83636221+ClanVv3@users.noreply.github.com>
2026-04-06 20:29:15 -0400

    Update SshClientUtilTest.java

--- java/com/amaze/filemanager/ssh/SshClientUtilTest.java
+++ java/com/amaze/filemanager/ssh/SshClientUtilTest.java
@@ -1,6 +1,6 @@
 package com.amaze.filemanager.ssh;
 
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.amaze.filemanager.filesystem.ssh.SshClientUtils;
 
@@ -14,48 +14,64 @@ import static org.junit.Assert.assertTrue;
 public class SshClientUtilTest {
 
     @Test
-    public void testEncryptDecryptUriWithNoPassword(){
+    public void testEncryptDecryptUriWithNoPassword() {
         String uri = "ssh://testuser@127.0.0.1:22";
-        assertEquals(uri, SshClientUtils.encryptSshPathAsNecessary(uri));
-        assertEquals(uri, SshClientUtils.decryptSshPathAsNecessary(uri));
+
+        String encrypted = SshClientUtils.encryptSshPathAsNecessary(uri);
+        String decrypted = SshClientUtils.decryptSshPathAsNecessary(encrypted);
+
+        assertEquals(uri, encrypted);
+        assertEquals(uri, decrypted);
     }
 
     @Test
-    public void testEncryptDecryptPasswordWithMinusSign1(){
+    public void testEncryptDecryptPasswordWithMinusSign1() {
         String uri = "ssh://testuser:abcd-efgh@127.0.0.1:22";
+
         String result = SshClientUtils.encryptSshPathAsNecessary(uri);
+
         assertTrue(result.contains("ssh://testuser:"));
         assertTrue(result.contains("@127.0.0.1:22"));
+
         String verify = SshClientUtils.decryptSshPathAsNecessary(result);
         assertEquals(uri, verify);
     }
 
     @Test
-    public void testEncryptDecryptPasswordWithMinusSign2(){
+    public void testEncryptDecryptPasswordWithMinusSign2() {
         String uri = "ssh://testuser:---------------@127.0.0.1:22";
+
         String result = SshClientUtils.encryptSshPathAsNecessary(uri);
+
         assertTrue(result.contains("ssh://testuser:"));
         assertTrue(result.contains("@127.0.0.1:22"));
+
         String verify = SshClientUtils.decryptSshPathAsNecessary(result);
         assertEquals(uri, verify);
     }
 
     @Test
-    public void testEncryptDecryptPasswordWithMinusSign3(){
+    public void testEncryptDecryptPasswordWithMinusSign3() {
         String uri = "ssh://testuser:--agdiuhdpost15@127.0.0.1:22";
+
         String result = SshClientUtils.encryptSshPathAsNecessary(uri);
+
         assertTrue(result.contains("ssh://testuser:"));
         assertTrue(result.contains("@127.0.0.1:22"));
+
         String verify = SshClientUtils.decryptSshPathAsNecessary(result);
         assertEquals(uri, verify);
     }
 
     @Test
-    public void testEncryptDecryptPasswordWithMinusSign4(){
+    public void testEncryptDecryptPasswordWithMinusSign4() {
         String uri = "ssh://testuser:t-h-i-s-i-s-p-a-s-s-w-o-r-d-@127.0.0.1:22";
+
         String result = SshClientUtils.encryptSshPathAsNecessary(uri);
+
         assertTrue(result.contains("ssh://testuser:"));
         assertTrue(result.contains("@127.0.0.1:22"));
+
         String verify = SshClientUtils.decryptSshPathAsNecessary(result);
         assertEquals(uri, verify);
     }
